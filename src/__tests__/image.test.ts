a9692e5c8ecdb62d1f320727a0ba7251da6cf1f7
Mike Farr <mike@noneleft.ca>
2023-07-16 16:31:55 -0400

    Implement generatePPMBody function

--- src/__tests__/image.test.ts
+++ src/__tests__/image.test.ts
@@ -1,22 +1,44 @@
 import { Canvas } from "../app/canvas";
 import { Image, toPPM } from "../app/image";
 
+let canvas: Canvas;
+
+beforeEach(() => {
+  canvas = new Canvas(5, 3);
+});
+
 test("generated PPMs should have the correct header", () => {
-  const canvas = new Canvas(5, 3);
   const image = new Image(canvas, 256);
 
-  const expected = `P3\n${canvas.width} ${canvas.height}\n${
-    image.colours - 1
-  }\n`;
+  const expected = `P3\n${canvas.width} ${canvas.height}\n${image.maxColour}`;
 
   const ppm = toPPM(image);
 
-  expect(ppm).toEqual(expected);
+  const header = ppm.split("\n").slice(0, 3).join("\n");
+
+  expect(header).toEqual(expected);
 });
 
 test("generated PPMs should be terminated by a newline character", () => {
-  const image = new Image(new Canvas(5, 3), 256);
+  const image = new Image(canvas, 256);
   const ppm = toPPM(image);
 
   expect(ppm.endsWith("\n")).toBe(true);
+});
+
+test("generated PPMs should have the correcy body", () => {
+  canvas.writePixel(0, 0, { r: 1.5, g: 0, b: 0 });
+  canvas.writePixel(2, 1, { r: 0, g: 0.5, b: 0 });
+  canvas.writePixel(4, 2, { r: -0.5, g: 0, b: 1 });
+
+  const image = new Image(canvas, 256);
+
+  const expected =
+    "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 128 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0 0 0 0 0 255";
+
+  const ppm = toPPM(image);
+
+  const body = ppm.split("\n").slice(3, 6).join("\n");
+
+  expect(body).toEqual(expected);
 });
--- src/app/image.ts
+++ src/app/image.ts
@@ -1,19 +1,57 @@
 import { Canvas } from "./canvas";
+import { Colour } from "./colour";
 
 export class Image {
+  public readonly maxColour: number;
+
   constructor(
-    public readonly canvas: Canvas,
+    private readonly canvas: Canvas,
     public readonly colours: number
-  ) {}
+  ) {
+    this.maxColour = this.colours - 1;
+  }
+
+  get height() {
+    return this.canvas.height;
+  }
+
+  get width() {
+    return this.canvas.width;
+  }
+
+  public pixelAt(w: number, h: number): Colour {
+    const pixel = this.canvas.pixelAt(w, h);
+
+    return {
+      r: this.clamp(Math.round(pixel.r * this.maxColour)),
+      g: this.clamp(Math.round(pixel.g * this.maxColour)),
+      b: this.clamp(Math.round(pixel.b * this.maxColour)),
+    };
+  }
+
+  public clamp(value: number): number {
+    return value < 0 ? 0 : value > this.maxColour ? this.maxColour : value;
+  }
 }
 
 export function toPPM(image: Image): string {
-  const ppm = `${generatePPMHeader(image)}\n`;
-  return ppm;
+  return `${generatePPMHeader(image)}\n${generatePPMBody(image)}\n`;
 }
 
 function generatePPMHeader(image: Image): string {
-  return `P3\n${image.canvas.width} ${image.canvas.height}\n${
-    image.colours - 1
-  }`;
+  return `P3\n${image.width} ${image.height}\n${image.maxColour}`;
+}
+
+function generatePPMBody(image: Image): string {
+  let body = "";
+
+  for (let h = 0; h < image.height; h++) {
+    for (let w = 0; w < image.width; w++) {
+      const c = image.pixelAt(w, h);
+      body += `${c.r} ${c.g} ${c.b} `;
+    }
+    body = body.trimEnd() + "\n";
+  }
+
+  return body.trimEnd();
 }
